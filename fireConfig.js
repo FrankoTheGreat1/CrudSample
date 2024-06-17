ff232ff5ec0e972c4d810aa5c196f2ccc4ee8670
FrankoTheGreat1 <144213654+FrankoTheGreat1@users.noreply.github.com>
2024-06-17 15:57:47 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCdJo-EpxRqQNnU1V_N2uXsUFHCAw_BDHk",
+    authDomain: "crud-a5201.firebaseapp.com",
+    databaseURL: "https://crud-a5201-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "crud-a5201",
+    storageBucket: "crud-a5201.appspot.com",
+    messagingSenderId: "772276684786",
+    appId: "1:772276684786:web:79a26814957c00a8480d6b",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
